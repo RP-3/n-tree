b262cfc5813f9c0c53cfffd01a7b2f658b2c1780
Rohan <sarith21@gmail.com>
2014-08-19 14:59:51 -0700

    Refactors tests to use standard mocha-should framework

--- test.js
+++ test.js
@@ -1,17 +1,26 @@
 var should   = require('should');
 var Tree     = require('./n-tree.js');
 
-var n = new Tree([1, 1], [0, 0], 2);
+var randomCoords = function(maxima, minima){
+  var result = new Array(maxima.length);
+  for(var i=0; i<maxima.length; i++){
+    result.push((Math.random()*(maxima[i]-minima[i])) + minima[i]);
+  }
+  return result;
+};
 
-n.getRelativeVector([0.6, 0.4]).should.equal(1);
+describe('getRelativeVector', function() {
+  n = new Tree([10, 10], [0, 0], 1);
+  it('should return the correct relative direction from a point', function() {
+    n.getRelativeVector([1, 1]).should.equal(0);
+    n.getRelativeVector([6, 1]).should.equal(1);
+    n.getRelativeVector([1, 6]).should.equal(2);
+    n.getRelativeVector([6, 6]).should.equal(3);
+  });
+});
 
-n = new Tree([10, 10, 10, 10], [0, 0, 0, 0], 1);
 
-n.getRelativeVector([3, 6, 5, 2]).should.equal(6);
-n.getRelativeVector([6, 6, 6, 6]).should.equal(15);
-
-
-n.insert([2, 4, 6, 8], "fortyFive!");
+/*n.insert([2, 4, 6, 8], "fortyFive!");
 //insert expectations
 
 
@@ -29,5 +38,5 @@ for(var k=0; k<100; k++){
 
 n.each([0.8, 0.6], [0.2, 0.5], function(item){
   console.log(item.coords, item.value);
-});
+});*/
 
